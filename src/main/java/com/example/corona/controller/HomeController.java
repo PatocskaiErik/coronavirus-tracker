68a8064d6fba1809b0f1cf34fad06f57dfe0f72e
Erik <patoerik93@gmail.com>
2022-02-21 19:18:14 +0100

    Added attribute

--- src/main/java/com/example/corona/controller/HomeController.java
+++ src/main/java/com/example/corona/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.example.corona.controller;
 
+import com.example.corona.service.DataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    DataService dataService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("locationStats", dataService.getAllStats());
         return "home";
     }
 }
