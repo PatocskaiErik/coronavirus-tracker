0d88f65d4eccf5cc16c0962427203bb35eb7e888
Erik <patoerik93@gmail.com>
2022-02-21 17:19:21 +0100

    Enabled scheduling

--- src/main/java/com/example/corona/CoronaApplication.java
+++ src/main/java/com/example/corona/CoronaApplication.java
@@ -2,8 +2,10 @@ package com.example.corona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaApplication {
 
 	public static void main(String[] args) {
