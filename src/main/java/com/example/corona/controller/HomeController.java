a741aa4d834841db919d1be02bbd71e46b4fd64d
Erik <patoerik93@gmail.com>
2022-02-21 19:52:32 +0100

    Added div

--- src/main/java/com/example/corona/controller/HomeController.java
+++ src/main/java/com/example/corona/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.corona.controller;
 
+import com.example.corona.models.LocationStats;
 import com.example.corona.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", dataService.getAllStats());
+        List<LocationStats> allStats = dataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+
         return "home";
     }
 }
